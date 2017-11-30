0a481e05683e8f6441fb762bc0be85e9968029f6
linceln <lince2030@gmail.com>
2017-11-30 20:55:04 +0800

    Vue

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -2,11 +2,16 @@
 
 const model = require('../model');
 
+var fn_todo_list_html = async (ctx, next) => {
+    ctx.render('todo.html');
+}
+
 var fn_todo_list = async (ctx, next) => {
     let Todo = model.Todo;
     var todoList = await Todo.findAll();
     ctx.rest({
         code: 1,
+        title: '待办事项',
         message: '获取成功',
         todoList: todoList
     })
@@ -29,6 +34,7 @@ var fn_create_todo = async (ctx, next) => {
 }
 
 module.exports = {
+    'GET /todo.html': fn_todo_list_html,
     'GET /api/v1/todos': fn_todo_list,
     'POST /api/v1/todos': fn_create_todo
 }
