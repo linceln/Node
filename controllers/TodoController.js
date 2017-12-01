6af22d660e502d1431ba8d0bc8c67c057f360b84
linceln <lince2030@gmail.com>
2017-12-01 23:39:58 +0800

    Todo

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -3,7 +3,9 @@
 const model = require('../model');
 
 var fn_todo_list_html = async (ctx, next) => {
-    ctx.render('todo.html');
+    ctx.render('todo.html', {
+        title: '待办事项'
+    });
 }
 
 var fn_todo_list = async (ctx, next) => {
@@ -11,7 +13,7 @@ var fn_todo_list = async (ctx, next) => {
     var todoList = await Todo.findAll();
     ctx.rest({
         code: 1,
-        title: '待办事项',
+        title: '待办事项列表',
         message: '获取成功',
         todoList: todoList
     })
@@ -19,6 +21,7 @@ var fn_todo_list = async (ctx, next) => {
 
 var fn_create_todo = async (ctx, next) => {
     let title = ctx.request.body.title;
+    console.log('title ' + title);
     let description = ctx.request.body.description;
     let rank = ctx.request.body.rank;
     let Todo = model.Todo;
@@ -29,12 +32,18 @@ var fn_create_todo = async (ctx, next) => {
     })
     ctx.rest({
         code: 1,
-        message: '成功新建一条待办事项'
+        message: '成功新建一条待办事项',
+        todo: newTodo
     })
+}
+
+var fn_delete_todo = async (ctx, next) => {
+
 }
 
 module.exports = {
     'GET /todo.html': fn_todo_list_html,
     'GET /api/v1/todos': fn_todo_list,
-    'POST /api/v1/todos': fn_create_todo
+    'POST /api/v1/todos': fn_create_todo,
+    'DELETE /api/v1/todos/:id': fn_delete_todo
 }
